80abf107bcd4d9c04bb18a76174a4ab4516b983a
babugeet <97796199+babugeet@users.noreply.github.com>
2024-09-18 17:42:53 +0000

    second commit

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 		fmt.Printf("Received DNS query from %v, %d bytes\n", clientAddr, n)
 
 		// Parse the DNS query
-		if err := parseDNSQuery(buffer[:n]); err != nil {
+		if err := parseDNSQuery(buffer[:n], conn, clientAddr); err != nil {
 			fmt.Printf("Error parsing DNS query: %v\n", err)
 			continue
 		}
@@ -42,7 +42,7 @@ func main() {
 }
 
 // parseDNSQuery parses the incoming DNS query
-func parseDNSQuery(data []byte) error {
+func parseDNSQuery(data []byte, conn *net.UDPConn, clientAddr *net.UDPAddr) error {
 	if len(data) < 12 {
 		return fmt.Errorf("invalid DNS query length")
 	}
@@ -78,8 +78,59 @@ func parseDNSQuery(data []byte) error {
 		fmt.Printf("Query Class: %d\n", qclass)
 
 		offset += 4
+		// Craft a DNS response
+		response := createDNSResponse(transactionID, domainName)
+
+		// Send the response to the client
+		_, err = conn.WriteToUDP(response, clientAddr)
+		if err != nil {
+			return fmt.Errorf("error sending DNS response: %v", err)
+		}
 	}
 	return nil
+}
+
+// createDNSResponse creates a DNS response based on the query
+func createDNSResponse(transactionID uint16, domainName string) []byte {
+	// DNS Header (12 bytes)
+	header := make([]byte, 12)
+	binary.BigEndian.PutUint16(header[0:2], transactionID) // Transaction ID
+	binary.BigEndian.PutUint16(header[2:4], 0x8180)        // Flags: Standard query response, no error
+	binary.BigEndian.PutUint16(header[4:6], 1)             // Number of questions
+	binary.BigEndian.PutUint16(header[6:8], 1)             // Number of answers
+	binary.BigEndian.PutUint16(header[8:10], 0)            // Number of authority RRs
+	binary.BigEndian.PutUint16(header[10:12], 0)           // Number of additional RRs
+
+	// Question Section
+	question := createDomainNameSection(domainName)     // The domain name section
+	question = append(question, 0x00, 0x01, 0x00, 0x01) // Type A, Class IN
+
+	// Answer Section
+	// The answer will be a simple static response. For example, returning an IP address "127.0.0.1"
+	answer := createDomainNameSection(domainName)   // Domain Name
+	answer = append(answer, 0x00, 0x01)             // Type A
+	answer = append(answer, 0x00, 0x01)             // Class IN
+	answer = append(answer, 0x00, 0x00, 0x00, 0x1e) // TTL (30 seconds)
+	answer = append(answer, 0x00, 0x04)             // Data length (4 bytes for IPv4 address)
+	answer = append(answer, 0x7f, 0x00, 0x00, 0x01) // IP address 127.0.0.1
+
+	// Combine all parts into the response
+	response := append(header, question...)
+	response = append(response, answer...)
+
+	return response
+}
+
+// createDomainNameSection creates the domain name section for the DNS message
+func createDomainNameSection(domainName string) []byte {
+	parts := strings.Split(domainName, ".")
+	var section []byte
+	for _, part := range parts {
+		section = append(section, byte(len(part)))
+		section = append(section, part...)
+	}
+	section = append(section, 0x00) // End of domain name
+	return section
 }
 
 // parseDomainName extracts the domain name from the DNS question section
